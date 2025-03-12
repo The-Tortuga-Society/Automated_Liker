f354e6f3397674db5460d3ad5426eb9188e452e0
ProfessorAxelrod <greenwriting@protonmail.com>
2025-03-12 14:43:37 -0700

    This code snippet is to be run in the browser console

---
+++ autoLikeArchives.js
@@ -0,0 +1,34 @@
+
+function autoLikeArchives() {
+    // Wait a bit for the page to fully load
+	var actionDelay = 2000;
+    setTimeout(() => {
+        // Find the like button - Substack typically uses a heart icon
+
+
+					var elList = document.querySelectorAll('a[aria-label*="Like"]');
+					elList.forEach(function(el) {
+						if (el.getAttribute('aria-pressed') === 'true' ) {
+							// this is already liked
+							   console.log('This is already liked');
+							   
+						} else {
+							// go ahead and like this one with delay
+							   actionDelay+=650;
+							   console.log('Not already liked');
+							   setTimeout(() => {
+							   console.log(el);
+							   el.click();
+						       console.log('Substack post liked!');
+							   }, actionDelay); // additional delay to ensure likes don't collide
+							   }
+						   
+					});					
+
+
+
+    }, actionDelay); // millisecond delay to ensure page elements are loaded
+}
+
+
+autoLikeArchives();
