a4dc9ec93611d82dde264aa3cb091176afb4ae09
Ubuntu <ubuntu@vps-f8f4309a.vps.ovh.us>
2025-03-14 16:52:01 +0000

    test

--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/github-deploy-key"
 
 python3 Liker_Feeder.py
+git status
 git pull origin main
 git add Liker_Data.json
 git commit -m "JSON data dump at $(date)"
 git push origin main
+git status
